00a7b1dbd6585e03d6f3f9b79e14bc762cd74572
donuhead7 <kennethdu3@gmail.com>
2019-04-26 11:16:26 -0700

    initial commit

---
+++ src/pages/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <h1>Contact Me!</h1>
+    <h2>These are the best ways to get into contact!</h2>
+    <ul>
+      <li>Email: kennethdu3@gmail.com</li>
+      <li><a href="https://www.linkedin.com/in/kenneth-du/" target="_blank" rel="noopener">LinkedIn</a></li>
+      <li><a href="https://www.github.com/kennethdu" target="_blank" rel="noopener">Github</a></li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Contact'
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <Layout>
+    
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
+    
+    <h1 class="center-text">Hello, welcome to my portfolio page!</h1>
+
+    <img src="../images/Profile.jpg" alt="Profile" id="profile">
+   
+    <p class="center-text">
+      Thank you for stopping by!
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+
+.center-text{
+  text-align: center;
+}
+
+#profile {
+  width: 25%;
+  height: 25%;
+  transform: rotate(90deg);
+  margin-left: 18rem;
+}
+</style>
